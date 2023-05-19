28d3a663ccf0a758bd4fc8fac06c9fabe0b31154
pikapika <yonghaoyang@qq.com>
2023-05-19 17:08:37 +0800

    实现MAR

---
+++ CPU.srcs/sources_1/new/MAR.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/05/19 16:55:33
+// Design Name: 
+// Module Name: MAR
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MAR (
+    input clk,
+    input rst,
+    input [15:0] C,
+    input [7:0] MBR_in,  //MBR[7:0]
+    input [7:0] PC_in,  // PC[7:0]
+
+    output [7:0] MAR_out  //to Memory
+);
+  reg [7:0] MAR_reg;
+  always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+      MAR_reg <= 0;
+
+    end else if (C[5] == 1) begin
+      MAR_reg <= MBR_in;
+    end else if (C[10==1]) begin
+      MAR_reg <= PC_in;
+    end else begin
+      MAR_reg <= MAR_reg;
+    end
+  end
+  assign MAR_out = MAR_reg;
+
+endmodule
